380090dc789c21cd847fbc25aa78722bc5d5907a
jlandis320 <jreynoldslandis@gmail.com>
2022-12-08 21:54:14 -0500

    decodes questions

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://opentdb.com/api.php?amount=10"
 
 export async function getGenQuestions(){
-  const res = await fetch(`${baseUrl}&category=9&difficulty=medium&type=multiple`)
+  const res = await fetch(`${baseUrl}&category=9&difficulty=medium&type=multiple&encode=base64`)
   return res.json()
 }
