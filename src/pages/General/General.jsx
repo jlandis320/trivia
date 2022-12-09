130b292bff598e7d0ef9b35f94c6973e8b9c3880
jlandis320 <jreynoldslandis@gmail.com>
2022-12-08 21:28:15 -0500

    adds questionsand answer buttons to general page

--- src/pages/General/General.jsx
+++ src/pages/General/General.jsx
@@ -19,6 +19,18 @@ const General = () => {
     <>
       <h1>I'm the general page</h1>
       <button onClick={() => navigate(-1)}>Go Back</button>
+
+      {questions.map(question => 
+      <section>
+        <h3>{question.question}</h3>
+        <button>{question.correct_answer}</button>
+        <button>{question.incorrect_answers[0]}</button>
+        <button>{question.incorrect_answers[1]}</button>
+        <button>{question.incorrect_answers[2]}</button>
+        </section>
+        ) }
+
+
     </>
   );
 };
