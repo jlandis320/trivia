30bd1c6bae492700f37d3c8f64b0beab38126a46
jlandis320 <jreynoldslandis@gmail.com>
2023-02-15 14:03:57 -0500

    question displays with shuffled answers

--- src/pages/General/General.jsx
+++ src/pages/General/General.jsx
@@ -4,34 +4,65 @@ import { useNavigate } from "react-router-dom";
 import { getGenQuestions } from "../../services/api-calls";
 
 const General = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [questions, setQuestions] = useState([]);
 
+  
   useEffect(() => {
     const fetchQuestions = async () => {
       const questionData = await getGenQuestions();
-      setQuestions(questionData.results);
+      setQuestions(questionData.results); 
     };
-    fetchQuestions();
+    fetchQuestions(); 
   }, []);
+  
+  let shuffledAnswers = questions.map(question => {
+    let answerArray = [question.incorrect_answers[0], question.correct_answer, question.incorrect_answers[1], question.incorrect_answers[2]] 
+    return shuffle(answerArray)
+  })
+  // Fisher-Yates shuffle to mix up answer options
+  function shuffle(array) {
+    var m = array.length,
+      t,
+      i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
+
+  let questionObj = questions.map((question, i) => {
+    return {
+    "question": question.question, 
+    "correct_answer": question.correct_answer, 
+    "answer_options": shuffledAnswers[i]
+  }
+  })
+  console.log(questionObj)
 
   return (
     <>
       <h1>I'm the general page</h1>
       <button onClick={() => navigate(-1)}>Go Back</button>
       <section>
-
-      {questions.map(question => 
-      <section>
-        <h3>{atob(question.question)}</h3>
-        <button>{atob(question.correct_answer)}</button>
-        <button>{atob(question.incorrect_answers[0])}</button>
-        <button>{atob(question.incorrect_answers[1])}</button>
-        <button>{atob(question.incorrect_answers[2])}</button>
-        </section>
-        ) }
-        </section>
-
+        {questionObj.map((question, idx) => (
+          <section key={idx}>
+            <h3>{question.question}</h3>
+            <button>{question.answer_options[0]}</button>
+            <button>{question.answer_options[1]}</button>
+            <button>{question.answer_options[2]}</button>
+            <button>{question.answer_options[3]}</button>
+          </section>
+        ))}
+      </section>
     </>
   );
 };
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,6 +1,9 @@
 const baseUrl = "https://opentdb.com/api.php?amount=10"
 
 export async function getGenQuestions(){
-  const res = await fetch(`${baseUrl}&category=9&difficulty=medium&type=multiple&encode=base64`)
+  const res = await fetch(`${baseUrl}&category=9&difficulty=medium&type=multiple`)
+  
   return res.json()
 }
+
+// &encode=base64
